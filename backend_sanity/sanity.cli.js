8c9a27ce713f15d4ce90b875987ef8dfd56efecc
Rajulorencemurmu <rajulorence4321@gmail.com>
2023-02-26 23:35:20 +0530

    first commit

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'eter9n2i',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,10 @@
+import testimonials from "./testimonials"
+import skills from "./skills"
+import work from "./work"
+import workExperience from "./workExperience"
+import experiences from "./experiences"
+import contact from "./contact"
+import brand from "./brand"
+import about from "./about"
+
+export const schemaTypes = [brand,skills,testimonials,work,workExperience,experiences,contact,about]
